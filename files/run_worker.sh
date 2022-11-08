86d119fc3f8878c4f0e5008ddc9e4050de2d51f7
Jiri Popelka <jpopelka@redhat.com>
2022-11-08 18:31:34 +0100

    Add shellcheck to pre-commit config

--- files/run_worker.sh
+++ files/run_worker.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
+# https://www.shellcheck.net/wiki/SC1091
+# shellcheck source=/dev/null
 source /usr/bin/setup_env_in_openshift.sh
 
-mkdir --mode=0700 -p "${PACKIT_HOME}/.ssh"
+mkdir -p "${PACKIT_HOME}/.ssh"
+chmod 0700 "${PACKIT_HOME}/.ssh"
+
 grep -q gitlab.com "${PACKIT_HOME}/.ssh/known_hosts" || ssh-keyscan gitlab.com >>"${PACKIT_HOME}/.ssh/known_hosts"
 
 run_worker_.sh
